115fc839dbb08ec31345cd2c87eeb5b41efaceb8
TD <tolueolasupo@gmail.com>
2026-01-08 11:04:16 +0000

    Added DirectDebit and StandingOrder classes to project

---
+++ src/DirectDebit.java
@@ -0,0 +1,16 @@
+public class DirectDebit {
+
+    private String payeeName;
+    private double amount;
+
+    public DirectDebit(String payeeName, double amount) {
+        this.payeeName = payeeName;
+        this.amount = amount;
+    }
+
+    public double getAmount() {return amount;}
+
+    public String getPayeeName() (return payeeName;)
+
+    public String toString() {return "Direct Debit to " + payeeName + " | Amount: " + String.format("£%.2f", amount);}
+}
