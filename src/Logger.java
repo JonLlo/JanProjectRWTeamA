f0e7c84ac2f8614c90236bfd3f7864a17075223b
goodChristina <eponymouswoman@gmail.com>
2026-01-09 01:26:01 +0000

    Update Logger.java

--- src/Logger.java
+++ src/Logger.java
@@ -1,3 +1,16 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Logger {
-    //
+
+    public static void main(String[] args) {
+        log("Logger is running");
+    }
+
+    public static void log(String message) {
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .format(new Date());
+        System.out.println(time + " - " + message);
+    }
 }
+
