e1b2e57dba07bacbce8451740147562209375d67
goodChristina <eponymouswoman@gmail.com>
2026-01-09 07:53:01 +0000

    changed log_file from public to private

--- src/Logger.java
+++ src/Logger.java
@@ -1,8 +1,11 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.io.*;
 
 public class Logger {
-
+    //needed main to run it separately
+    // change main/log when all the files are done
+    private static final String LOG_FILE = "bank_log.txt";
     public static void main(String[] args) {
         log("Logger is running");
     }
