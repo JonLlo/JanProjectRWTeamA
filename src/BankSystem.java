379a51c206f049c5e98140a98455fa537f9230ef
goodChristina <eponymouswoman@gmail.com>
2026-01-11 02:51:55 +0000

    changed BankSystem slightly so the skeleton runs on its own in terminal

--- src/BankSystem.java
+++ src/BankSystem.java
@@ -1,194 +1,123 @@
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-
 public class BankSystem {
-    // private Customer loggedInCustomer;  //Here we must define a 'customer' class.
-    // private Map<String, Customer> customerMap = new HashMap<>(); //hash map to log users. Also relies on a 'customer' class.
-    private Scanner inputScanner = new Scanner(System.in); //Scanner for "start()" method
-    /*The following constructor "public BankSystem()" will run whenever a new object of the class BankSystem is created:
-    i.e. when "BankSystem bank = new BankSystem();" is called */
-    public BankSystem() {
-        //IO.println("\n=== Bank System has been called! ===");
 
+    private Scanner inputScanner = new Scanner(System.in);
+
+    public BankSystem() {
+        System.out.println("\n=== Bank System Initialized ===");
     }
+
     public void start() {
         boolean running = true;
         while (running) {
-            IO.println("========================================\n" +
-                    "       ACME BANK System\n" +
-                    "========================================");
-            IO.println("Please select an option:");
-            IO.println("1. Authenticate Customer");
-            IO.println("2. Create New Customer");
-            IO.println("3. Help");
-            IO.println("4. Exit");
-
-            IO.println("Tip: You can type help at any time for assistance.");
-
-            IO.print("Enter Option number: ");
+            System.out.println("========================================");
+            System.out.println("       ACME BANK System");
+            System.out.println("========================================");
+            System.out.println("Please select an option:");
+            System.out.println("1. Authenticate Customer");
+            System.out.println("2. Create New Customer");
+            System.out.println("3. Help");
+            System.out.println("4. Exit");
+            System.out.print("Enter Option number: ");
 
             String menuChoice = inputScanner.nextLine();
 
             switch (menuChoice) {
-                case "1": IO.println("authenticateCustomer()"); break;
-                case "2": createCustomer(); break;
-                case "3": IO.println("showMainHelp()"); break;
-                case "4": IO.println("saveDataToCSV()"); running = false; break;
-                default: IO.println("Invalid choice.");
+                case "1":
+                    authenticateCustomer();
+                    break;
+                case "2":
+                    createCustomer();
+                    break;
+                case "3":
+                    showMainHelp();
+                    break;
+                case "4":
+                    System.out.println("Exiting the system. Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
             }
-
-
-
-
-
-
         }
     }
+
     private void authenticateCustomer() {
-        /* Here we want to write a function to authenticate a customer. This is called when someone
-            types '1' on the start menu */
-        /* We will then want to call a function "CustomerMenu()" which will take user to the customer menu */
-
-
-        //some code then:
-        //CustomerMenu()
-
+        System.out.println("authenticateCustomer() called");
+        // Stub: would normally check login
+        customerMenu(); // demo navigation
     }
+
     private void createCustomer() {
-        /* Here we want to write a function to create a customer. This is called when someone
-        types '2' on the start menu */
-
-        IO.print("Enter Customer ID: ");
+        System.out.println("createCustomer() called");
+        // Stub: normally get input and create Customer object
+        System.out.print("Enter Customer ID: ");
         String customerId = inputScanner.nextLine();
-
-        if (customerMap.containsKey(customerId)) {
-            IO.println("Customer ID already exists.");
-            return;
-        }
-
-        IO.print("Enter Customer Name: ");
+        System.out.print("Enter Customer Name: ");
         String customerName = inputScanner.nextLine();
 
-        customerMap.put(customerId, new Customer(customerId, customerName));
         Logger.log("NEW CUSTOMER CREATED: " + customerId);
-
-        IO.println("Customer created successfully.");
-        saveDataToCSV();
+        System.out.println("Customer created successfully (stub).");
     }
+
     private void showMainHelp() {
-       /* Here we want to write a function to create a customer. This is called when someone
-        types '3' on the start menu. It also, as per the brief, needs to be called anytime someone types 'help'
-        ANYWHERE in the application*/
-    }
-    private void saveDataToCSV() {
-        /*Here we want to write a function that saves all the data to the CSV (essentially the hashmap but in plain text).
-        And we want to run this just before the program is exited (when someone types '4')
-         */
-
+        System.out.println("showMainHelp() called");
+        System.out.println("This is a stub help menu for ACME BANK System.");
     }
 
     private void customerMenu() {
         boolean stayInCustomerMenu = true;
         while (stayInCustomerMenu) {
-            IO.println("\n=== CUSTOMER MENU ===");
-            IO.println("1. View Accounts");
-            IO.println("2. Open Account");
-            IO.println("3. Deposit");
-            IO.println("4. Withdraw");
-            IO.println("5. Direct Debit / Standing Order");
-            IO.println("6. Help");
-            IO.println("7. Switch Customer");
-
+            System.out.println("\n=== CUSTOMER MENU ===");
+            System.out.println("1. View Accounts");
+            System.out.println("2. Open Account");
+            System.out.println("3. Deposit");
+            System.out.println("4. Withdraw");
+            System.out.println("5. Direct Debit / Standing Order");
+            System.out.println("6. Help");
+            System.out.println("7. Switch Customer");
+            System.out.print("Enter Option number: ");
 
             String choice = inputScanner.nextLine();
 
             switch (choice) {
-                //below are all functions we need to define later on
-                case "1": //loggedInCustomer.viewAccounts() //break;
-                    //"ViewAccounts" method needs to be defined in customer class.
-                case "2": //openNewAccount(); break;
-                case "3": //performDeposit(); break;
-                case "4": //performWithdrawal(); break;
-                case "5": //managePayments(); break;
-                case "6": //showCustomerHelp(); break;
-                case "7": stayInCustomerMenu = false; break;
-                default: IO.println("Invalid choice.");
+                case "1":
+                    System.out.println("View Accounts stub");
+                    break;
+                case "2":
+                    System.out.println("Open Account stub");
+                    break;
+                case "3":
+                    System.out.println("Deposit stub");
+                    break;
+                case "4":
+                    System.out.println("Withdraw stub");
+                    break;
+                case "5":
+                    System.out.println("Manage Payments stub");
+                    break;
+                case "6":
+                    System.out.println("Customer Help stub");
+                    break;
+                case "7":
+                    System.out.println("Returning to main menu...");
+                    stayInCustomerMenu = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
             }
         }
     }
-    private void openNewAccount() {
-        /*
-         - here we define a function to open a new account.
-         - will be dependent on an account class and a customer class.
-         - Note diff types of account (ISA, business)
-         - called when user types "2" when on customer menu
 
-         */
+    // Stubs for future functionality
+    private void openNewAccount() {}
+    private void performDeposit() {}
+    private void performWithdrawal() {}
+    private void managePayments() {}
+
+    public static void main(String[] args) {
+        BankSystem bankSystem = new BankSystem();
+        bankSystem.start();
     }
-    private void performDeposit() {
-        /*
-         - here we define a function to put down a deposit.
-         - Must call methods that will be in the account class,
-           namely putting down a deposit on a particular account
-         - Note diff types of account (ISA, business)
-         - called when user types "3" when on customer menu */
-    }
-    private void performWithdrawal() {
-        /*
-         - Similar process to the 'performDeposit' function
-         - called when user types "4" when on customer menu */
-    }
-    private void managePayments() {
-     /*
-         - here we define a function to manage payments.
-         - will be dependent on an account class and a customer class.
-         - Note diff types of account (personal, ISA, business)
-         - called when user types "2" when on customer menu
-
-         */
-        IO.print("Enter account number: ");
-        /* now write some code to check that account is a PERSONAL Account,
-         on system and if so print the below:
-         */
-
-
-
-        IO.println("\n=== DIRECT DEBITS & STANDING ORDERS ===");
-        IO.println("1. Add Direct Debit");
-        IO.println("2. View Direct Debits");
-        IO.println("3. Add Standing Order");
-        IO.println("4. View Standing Orders");
-        IO.println("5. Back");
-
-        switch (inputScanner.nextLine()) {
-            //case "1": addDirectDebit(personalAccount); break;
-            //case "2": personalAccount.viewDirectDebits(); break;
-            //case "3": addStandingOrder(personalAccount);
-            //case "4": personalAccount.viewStandingOrders(); break;
-            //note viewDirectDebits() and viewStandingOrders() will rely on the account class.
-            //addStandingOrder() and addDirectDebit() will be defined on this document next.
-            case "5": return;
-            default: IO.println("Invalid choice.");
-        }
-  /*
-        private void addDirectDebit(PersonalAccount account) {
-            here we will want to add a direct debit to a given personalAccount}
-   */
-
-     /*
-    private void addStandingOrder(PersonalAccount account) {
-           here we will want to add a standing order to a given personalAccount
-    }
-    */
-
-
-    }
-
-
-
-
-
 }
