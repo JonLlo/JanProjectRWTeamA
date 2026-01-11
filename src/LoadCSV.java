5d9099e395ddfdc4439d24fbc7ec314c5921969a
goodChristina <eponymouswoman@gmail.com>
2026-01-11 01:25:35 +0000

    added comment

--- src/LoadCSV.java
+++ src/LoadCSV.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+//This, or something similar, will be added to BankSystem.java
+
 public class LoadCSV {
 
     public static void main(String[] args) {
