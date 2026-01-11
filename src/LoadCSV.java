87afbbb5b656b3ebc348c130e5e3fdc13ffeb133
goodChristina <eponymouswoman@gmail.com>
2026-01-11 01:22:09 +0000

    CSV method class for BankSystem

---
+++ src/LoadCSV.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LoadCSV {
+
+    public static void main(String[] args) {
+        loadDataFromCSV();
+    }
+
+    public static void loadDataFromCSV() {
+        System.out.println("Loading CSV data...");
+
+        //
+    }
+}
