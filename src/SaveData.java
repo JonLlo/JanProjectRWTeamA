fd976eed13e57a7d129b98c63d192a1da9405e9b
goodChristina <eponymouswoman@gmail.com>
2026-01-13 02:41:33 +0000

    Creating SaveDataToCSV() section

---
+++ src/SaveData.java
@@ -0,0 +1,11 @@
+public class SaveData {
+
+    public void SaveDataToCSV() {
+        System.out.println("Test");
+    }
+
+    public static void main() {
+        SaveData sd = new SaveData();
+        sd.SaveDataToCSV();
+    }
+}
