b00ed8c12e651e213f8fbbb6b6b486a7aae7c1e5
Kevin Hyde-Opoku <kevhydeopoku@gmail.com>
2026-01-23 21:19:45 +0000

    international payment

---
+++ src/InternationalPayment.java
@@ -0,0 +1,23 @@
+public class InternationalPayment {
+
+    private double foreignAmount;
+    private double exchangeRate;
+    private double gbpAmount;
+
+    public InternationalPayment(double foreignAmount, double exchangeRate) {
+        this.foreignAmount = foreignAmount;
+        this.exchangeRate = exchangeRate;
+        this.gbpAmount = foreignAmount * exchangeRate;
+    }
+
+    public double getForeignAmount() { return foreignAmount; }
+    public double getExchangeRate() { return exchangeRate; }
+    public double getGbpAmount() { return gbpAmount; }
+
+    @Override
+    public String toString() {
+        return "International Payment | Foreign: " + foreignAmount +
+                " | Rate: " + exchangeRate +
+                " | GBP: £" + String.format("%.2f", gbpAmount);
+    }
+}
